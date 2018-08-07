e99d7f153826e5ad176f812b88b7ded6b85af0c8
李智 <zhi.li@whats-egg.com>
2018-08-07 15:01:47 +0800

    初始化项目

---
+++ src/main/java/javalearning/javalearning/Test/Test.java
@@ -0,0 +1,14 @@
+package javalearning.javalearning.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: liz
+ * @Date: 2018/8/7
+ * @Time: 14:56
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("214");
+    }
+}

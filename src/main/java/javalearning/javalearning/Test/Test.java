8b548ab91b0a76105cbb6a700230b62ee1228f71
李智 <zhi.li@whats-egg.com>
2018-08-07 15:23:46 +0800

    测试项目

--- src/main/java/javalearning/javalearning/Test/Test.java
+++ src/main/java/javalearning/javalearning/Test/Test.java
@@ -9,6 +9,6 @@ package javalearning.javalearning.Test;
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("214");
+        System.out.println("hello Mr.Zhou!");
     }
 }
---
+++ src/main/java/javalearning/javalearning/mapper/BookMapper.java
@@ -0,0 +1,11 @@
+package javalearning.javalearning.mapper;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: liz
+ * @Date: 2018/8/7
+ * @Time: 15:19
+ */
+public class BookMapper {
+}

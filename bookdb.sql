e4ffa25d12a71d2d6cc35a3b95faf257c65b2e67
李智 <zhi.li@whats-egg.com>
2018-08-08 15:10:01 +0800

    规范了表名

--- bookdb.sql
+++ bookdb.sql
@@ -11,17 +11,17 @@
  Target Server Version : 50722
  File Encoding         : 65001
 
- Date: 07/08/2018 16:09:21
+ Date: 08/08/2018 15:07:54
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for shop_book
+-- Table structure for lib_Book
 -- ----------------------------
-DROP TABLE IF EXISTS `shop_book`;
-CREATE TABLE `shop_book` (
+DROP TABLE IF EXISTS `lib_Book`;
+CREATE TABLE `lib_Book` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `author` varchar(10) DEFAULT NULL,
@@ -35,6 +35,14 @@ CREATE TABLE `shop_book` (
   `updateTime` datetime DEFAULT NULL,
   `updateBy` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1
+
+-- ----------------------------
+-- Table structure for lib_Member
+-- ----------------------------
+DROP TABLE IF EXISTS `lib_Member`;
+CREATE TABLE `lib_Member` (
+  `name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
